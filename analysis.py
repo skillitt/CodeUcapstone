291631ecb46a65866587c8335916bfd71b8fe75b
skillitt <markdevore@outlook.com>
2024-07-07 13:56:19 -0400

    edited commits

--- analysis.py
+++ analysis.py
@@ -4,11 +4,11 @@ import pandas as pd
 attacks_file = "attacks.csv"
 threats_file = "threats.csv"
 
-# Load datasets
+# datasets
 attacks_data = pd.read_csv(attacks_file)
 threats_data = pd.read_csv(threats_file)
 
-# Example: Print first few rows of each dataset
+# Print first few rows of each dataset
 print("Attacks Data:")
 print(attacks_data.head())
 
